9477a2298e8bb115ba61584dec570d8d44fddb3a
Mark Sobkow <mark.sobkow@gmail.com>
2026-01-16 05:37:14 -0600

    2026-01-16 05:35 I believe the remapping of packages from server.markhome.mcf to io.github.msobkow is complete.

---
+++ src/main/java/io/github/msobkow/v3_1/cfint/cfintsaxramldr/CFIntSaxRamLdr.java
@@ -0,0 +1,136 @@
+// Description: Java 25 XML SAX Parser for CFInt.
+
+/*
+ *	io.github.msobkow.CFInt
+ *
+ *	Copyright (c) 2016-2026 Mark Stephen Sobkow
+ *	
+ *	Mark's Code Fractal 3.1 CFInt - Internet Essentials
+ *	
+ *	This file is part of Mark's Code Fractal CFInt.
+ *	
+ *	Mark's Code Fractal CFInt is available under dual commercial license from
+ *	Mark Stephen Sobkow, or under the terms of the GNU Library General Public License,
+ *	Version 3 or later.
+ *	
+ *	Mark's Code Fractal CFInt is free software: you can redistribute it and/or
+ *	modify it under the terms of the GNU Library General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	Mark's Code Fractal CFInt is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU Library General Public License
+ *	along with Mark's Code Fractal CFInt.  If not, see <https://www.gnu.org/licenses/>.
+ *	
+ *	If you wish to modify and use this code without publishing your changes in order to
+ *	tie it to proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ *	
+ */
+
+package io.github.msobkow.v3_1.cfint.cfintsaxramldr;
+
+import org.apache.log4j.*;
+import io.github.msobkow.v3_1.cflib.*;
+import io.github.msobkow.v3_1.cflib.xml.*;
+import io.github.msobkow.v3_1.cfsec.cfsec.*;
+import io.github.msobkow.v3_1.cfint.cfint.*;
+
+import io.github.msobkow.v3_1.cfsec.cfsecobj.*;
+import io.github.msobkow.v3_1.cfint.cfintobj.*;
+import io.github.msobkow.v3_1.cfint.CFIntRam.*;
+import io.github.msobkow.v3_1.cfint.CFIntSaxLoader.*;
+
+public class CFIntSaxRamLdr
+	extends CFIntSaxLdr
+{
+	private static ICFLibMessageLog log = new CFLibConsoleMessageLog();
+
+	// Constructors
+
+	public CFIntSaxRamLdr() {
+		super( log );
+	}
+
+	// main() entry point
+
+	public static void main( String args[] ) {
+		final String S_ProcName = "CFIntSaxRamLdr.main() ";
+		initConsoleLog();
+		int numArgs = args.length;
+		if( numArgs >= 2 ) {
+			ICFIntSchema cFIntSchema = new CFIntRamSchema();
+			ICFIntSchemaObj cFIntSchemaObj = new CFIntSchemaObj();
+			cFIntSchemaObj.setBackingStore( cFIntSchema );
+			CFIntSaxLdr cli = new CFIntSaxRamLdr();
+			CFIntSaxLoader loader = cli.getSaxLoader();
+			loader.setSchemaObj( cFIntSchemaObj );
+			String url = args[1];
+			try {
+				cFIntSchema.connect( "system", "system", "system", "system" );
+				cFIntSchema.rollback();
+				cFIntSchema.beginTransaction();
+				cFIntSchemaObj.setSecCluster( cFIntSchemaObj.getClusterTableObj().getSystemCluster() );
+				cFIntSchemaObj.setSecTenant( cFIntSchemaObj.getTenantTableObj().getSystemTenant() );
+				cFIntSchemaObj.setSecSession( cFIntSchemaObj.getSecSessionTableObj().getSystemSession() );
+				CFSecAuthorization auth = new CFSecAuthorization();
+				auth.setSecCluster( cFIntSchemaObj.getSecCluster() );
+				auth.setSecTenant( cFIntSchemaObj.getSecTenant() );
+				auth.setSecSession( cFIntSchemaObj.getSecSession() );
+				cFIntSchemaObj.setAuthorization( auth );
+				loader.setUseCluster( cFIntSchemaObj.getSecCluster() );
+				loader.setUseTenant( cFIntSchemaObj.getSecTenant() );
+				applyLoaderOptions( loader, args[0] );
+				cli.evaluateRemainingArgs( args, 2 );
+				loader.parseFile( url );
+				cFIntSchema.commit();
+				cFIntSchema.disconnect( true );
+			}
+			catch( Exception e ) {
+				log.message( S_ProcName + "EXCEPTION: Could not parse XML file \"" + url + "\": " + e.getMessage() );
+				e.printStackTrace( System.out );
+			}
+			catch( Error e ) {
+				log.message( S_ProcName + "ERROR: Could not parse XML file \"" + url + "\": " + e.getMessage() );
+				e.printStackTrace( System.out );
+			}
+			finally {
+				if( cFIntSchema.isConnected() ) {
+					cFIntSchema.rollback();
+					cFIntSchema.disconnect( false );
+				}
+			}
+		}
+		else {
+			log.message( S_ProcName + "ERROR: Expected at least two argument specifying the loader options and the name of the XML file to parse.  The first argument may be empty." );
+		}
+	}
+
+	// Initialize the console log
+
+	protected static void initConsoleLog() {
+//		Layout layout = new PatternLayout(
+//				"%d{ISO8601}"		// Start with a timestamp
+//			+	" %-5p"				// Include the severity
+//			+	" %C.%M"			// pkg.class.method()
+//			+	" %F[%L]"			// File[lineNumber]
+//			+	": %m\n" );			// Message text
+//		BasicConfigurator.configure( new ConsoleAppender( layout, "System.out" ) );
+	}
+
+	// Evaluate remaining arguments
+
+	public void evaluateRemainingArgs( String[] args, int consumed ) {
+		// There are no extra arguments for the RAM "database" instance
+		if( args.length > consumed ) {
+			log.message( "CFIntSaxRamLdr.evaluateRemainingArgs() WARNING No extra arguments are expected for a RAM database instance, but "
+				+ Integer.toString( args.length - consumed )
+				+ " extra arguments were specified.  Extra arguments ignored." );
+		}
+	}
+
+}
